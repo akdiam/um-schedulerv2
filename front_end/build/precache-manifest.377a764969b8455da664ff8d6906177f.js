07b461b08e20429e61abd157c37da9f163cf70f6
Adam Kim <adamkim@umich.edu>
2020-08-04 15:47:21 -0400

    edited proxy

---
+++ front_end/build/precache-manifest.377a764969b8455da664ff8d6906177f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a527c363b87b11cab26f3065522e566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "464ec931a011fac317df",
+    "url": "/static/css/2.d34109c7.chunk.css"
+  },
+  {
+    "revision": "39c9e1b031a595490efa",
+    "url": "/static/css/main.48e60b39.chunk.css"
+  },
+  {
+    "revision": "464ec931a011fac317df",
+    "url": "/static/js/2.d72870d2.chunk.js"
+  },
+  {
+    "revision": "b846b5b2fcb30d4eb0a413c317540089",
+    "url": "/static/js/2.d72870d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39c9e1b031a595490efa",
+    "url": "/static/js/main.ee498de1.chunk.js"
+  },
+  {
+    "revision": "87df155a2536392dc122",
+    "url": "/static/js/runtime-main.5e37709c.js"
+  }
+]);
